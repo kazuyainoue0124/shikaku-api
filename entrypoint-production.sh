d4b1b67f014deabf94dbbc737c59f94cc4c0d6df
kazuyainoue0124 <bauer24tbp@gmail.com>
2023-01-19 00:34:21 +0900

    開発環境修正

--- entrypoint-production.sh
+++ entrypoint-production.sh
@@ -4,5 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /shikaku/tmp/pids/server.pid
 
+# ECS用に追記
+# bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
