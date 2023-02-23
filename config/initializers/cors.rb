cdbc03ccf987d4af17aff6f41515cae6ee315241
kazuyainoue0124 <bauer24tbp@gmail.com>
2023-02-23 23:32:12 +0900

    VercelのURLをcors.rbに追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8000',
-            'http://localhost:3000'
+            'http://localhost:3000',
+            'https://shikaku.dev/'
 
     resource '*',
             headers: :any,
