e028e2cd67bc7d9ffe5d9a7ab933a05e5059277c
kazuyainoue0124 <bauer24tbp@gmail.com>
2023-02-28 10:11:23 +0900

    テストコードの作成

--- spec/requests/api/v1/certificates_spec.rb
+++ spec/requests/api/v1/certificates_spec.rb
@@ -1,7 +1,14 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Api::V1::Certificates", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe "Api::V1::Certificates", type: :request do
+  describe "GET /index" do
+    let!(:certificate1) { create(:certificate) }
+    let!(:certificate2) { create(:certificate) }
+
+    it '資格データを全件取得する' do
+      get '/api/v1/certificates'
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['certificates'].size).to eq(2)
+    end
+  end
+end
